672171213d9783f656196a4fae1d25065d980792
strawberrymilktea0604 <4004267@st.huce.edu.vn>
2025-12-15 01:30:07 +0700

    update

--- HealthSync.WebApi/Program.cs
+++ HealthSync.WebApi/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.HttpsPolicy;
 using System.Text;
 using System.Collections.Generic;
 using HealthSync.Domain.Entities;
